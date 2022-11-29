3a9b2a4ee5e34964f0689e0f7f85ca99dedabb9f
Maxim <makfedorovtooo@gmail.com>
2022-11-29 20:43:02 +0300

    All done

--- program_1/Program.cs
+++ program_1/Program.cs
@@ -1,1 +1,6 @@
-﻿number = Console.ReadLine();
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Put your number ");
+int addNumb = int.Parse(Console.ReadLine());
+int result = addNumb * addNumb;
+Console.WriteLine($"Квадрат числа равен {result}");
